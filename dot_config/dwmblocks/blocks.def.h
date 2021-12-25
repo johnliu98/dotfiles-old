97c7bb547b59b29b1d33cbab9486beceadfab676
John Liu <johnliu@kth.se>
2021-12-25 22:34:18 +0100

    feat(dwmblocks): add statusbar

---
+++ dot_config/dwmblocks/blocks.def.h
@@ -0,0 +1,11 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"BAT:",    "sb-battery",					                                  60,		 0},
+	{"CLOCK:",  "sb-clock",					                                     5,		 0},
+	{"VOL:",    "sb-volume",					                                   0,		10},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
