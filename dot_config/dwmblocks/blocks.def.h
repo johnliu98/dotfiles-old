d1f62e2f1834a75b924e257ad43a22cc35333009
John Liu <johnliu@kth.se>
2021-12-26 14:52:33 +0100

    feat(dwmblocks): add forecast, cpu and wifi

--- dot_config/dwmblocks/blocks.def.h
+++ dot_config/dwmblocks/blocks.def.h
@@ -1,11 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"BAT:",    "sb-battery",					                                  60,		 0},
-	{"CLOCK:",  "sb-clock",					                                     5,		 0},
-	{"VOL:",    "sb-volume",					                                   0,		10},
+	{"",    "sb-forecast",					                                  1800,		 0},
+	{"",    "sb-cpu",					                                          60,		 0},
+	{"",    "sb-internet",					                                     5,		 0},
+	{"",    "sb-battery",					                                      60,		 0},
+	{"",    "sb-clock",					                                         5,		 0},
+	{"",    "sb-volume",					                                       0,		10},
+	{"",    "sb-help-icon",					                                     0,		15},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
